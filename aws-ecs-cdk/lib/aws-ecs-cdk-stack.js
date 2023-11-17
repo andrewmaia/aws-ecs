e267ffa204c1f84ac98d1b046aed9ffb1b111e80
Andrew Maia <andrewsmaia@gmail.com>
2023-11-16 23:02:25 -0300

    Pipeline

--- aws-ecs-cdk/lib/aws-ecs-cdk-stack.js
+++ aws-ecs-cdk/lib/aws-ecs-cdk-stack.js
@@ -6,6 +6,10 @@ const ecs = require("aws-cdk-lib/aws-ecs");
 const cdk = require("aws-cdk-lib/core");
 const iam = require("aws-cdk-lib/aws-iam");
 const ecsPatterns = require("aws-cdk-lib/aws-ecs-patterns");
+const codebuild = require("aws-cdk-lib/aws-codebuild");
+const codepipeline = require("aws-cdk-lib/aws-codepipeline");
+const s3 = require("aws-cdk-lib/aws-s3");
+const codepipeline_actions = require("aws-cdk-lib/aws-codepipeline-actions");
 
 class AwsEcsCdkStack extends Stack {
   constructor(scope, id, props) {
@@ -97,9 +101,6 @@ class AwsEcsCdkStack extends Stack {
           loadBalancerName: "LoadBalancerEcsTeste",
           minHealthyPercent: 100,
           maxHealthyPercent: 200,
-          deploymentController: {
-            type: ecs.DeploymentControllerType.CODE_DEPLOY,
-          },
         }
       );
 
@@ -116,6 +117,200 @@ class AwsEcsCdkStack extends Stack {
 
     new cdk.CfnOutput(this, "UrlLoadbalancerAcessarSite", {
       value: fargateLoadBalancedService.loadBalancer.loadBalancerDnsName,
+    });
+
+    ////////////****Pipeline*******////////////////
+
+    //ArtifactBucket
+    const artifactBucket = new s3.Bucket(this, "Bucket", {
+      bucketName: "artifactbucketecsteste",
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    //Codebuild
+    const codeBuildServiceRole = new iam.Role(this, "CodeBuildServiceRole", {
+      roleName: "CodeBuildServiceRoleEcsTeste",
+      assumedBy: new iam.ServicePrincipal("codebuild.amazonaws.com"),
+    });
+
+    codeBuildServiceRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        resources: ["*"],
+        actions: [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "ecr:GetAuthorizationToken",
+        ],
+      })
+    );
+
+    codeBuildServiceRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        resources: ["*"],
+        actions: ["s3:GetObject", "s3:PutObject", "s3:GetObjectVersion"],
+      })
+    );
+
+    codeBuildServiceRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        resources: ["*"],
+        actions: [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+        ],
+      })
+    );
+
+    const codebuildProject = new codebuild.PipelineProject(
+      this,
+      "CodeBuildProject",
+      {
+        projectName: "CodeBuildProjectEcsTeste",
+        role: codeBuildServiceRole,
+
+        buildSpec: codebuild.BuildSpec.fromObject({
+          version: "0.2",
+          phases: {
+            install: {
+              runtime_versions: "python: 3.11",
+              commands: [
+                "pip install --upgrade pip",
+                "pip install --upgrade awscli aws-sam-cli",
+              ],
+            },
+            pre_build: {
+              commands: [
+                "$(aws ecr get-login --no-include-email)",
+                'TAG="$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | head -c 8)"',
+                'IMAGE_URI="${REPOSITORY_URI}:${TAG}"',
+              ],
+            },
+            build: {
+              commands: ['docker build --tag "$IMAGE_URI" .'],
+            },
+            post_build: {
+              commands: [
+                'docker push "$IMAGE_URI"',
+                'printf \'[{"name":"ContainerEcsTeste","imageUri":"%s"}]\' "$IMAGE_URI" > images.json',
+              ],
+            },
+          },
+          artifacts: { files: "images.json" },
+        }),
+        environment: {
+          computeType: codebuild.ComputeType.SMALL,
+          buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_5,
+          privileged: true,
+        },
+        environmentVariables: {
+          AWS_DEFAULT_REGION: { value: cdk.Stack.of(this).region },
+          REPOSITORY_URI: {
+            value:
+              "884588048908.dkr.ecr.us-east-1.amazonaws.com/repositoryecsteste",
+          },
+        },
+      }
+    );
+
+    //Pipeline
+
+    const codePipelineServiceRole = new iam.Role(
+      this,
+      "CodePipelineServiceRole",
+      {
+        roleName: "CodePipelineServiceRoleEcsTeste",
+        assumedBy: new iam.ServicePrincipal("codepipeline.amazonaws.com"),
+      }
+    );
+
+    codePipelineServiceRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        resources: ["*"],
+        actions: [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:GetObjectVersion",
+          "s3:GetBucketVersioning",
+        ],
+      })
+    );
+
+    codePipelineServiceRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        resources: ["*"],
+        actions: [
+          "ecs:DescribeServices",
+          "ecs:DescribeTaskDefinition",
+          "ecs:DescribeTasks",
+          "ecs:ListTasks",
+          "ecs:RegisterTaskDefinition",
+          "ecs:UpdateService",
+          "codebuild:StartBuild",
+          "codebuild:BatchGetBuilds",
+          "iam:PassRole",
+        ],
+      })
+    );
+
+    const pipeline = new codepipeline.Pipeline(this, "Pipeline", {
+      pipelineName: "pipelineEcsTeste",
+      artifactBucket: artifactBucket,
+      role: codePipelineServiceRole,
+    });
+
+    //Source
+    const sourceOutput = new codepipeline.Artifact();
+    const sourceAction =
+      new codepipeline_actions.CodeStarConnectionsSourceAction({
+        actionName: "SourceAction",
+        owner: "andrewmaia",
+        repo: "aws-ecs",
+        connectionArn:
+          "arn:aws:codestar-connections:us-east-1:884588048908:connection/edb57b04-d802-447e-b543-685ddc50379b",
+
+        output: sourceOutput,
+      });
+
+    pipeline.addStage({
+      stageName: "Source",
+      actions: [sourceAction],
+    });
+
+    //Build
+    const buildOutput = new codepipeline.Artifact("BuildArtifact");
+    const buildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: "BuildAction",
+      project: codebuildProject,
+      input: sourceOutput,
+      outputs: [buildOutput],
+    });
+
+    pipeline.addStage({
+      stageName: "Build",
+      actions: [buildAction],
+    });
+
+    //Deploy
+    const deployAction = new codepipeline_actions.EcsDeployAction({
+      actionName: "DeployAction",
+      service: fargateLoadBalancedService.service,
+      imageFile: buildOutput.atPath("images.json"),
+    });
+
+    pipeline.addStage({
+      stageName: "Deploy",
+      actions: [deployAction],
     });
   }
 }
